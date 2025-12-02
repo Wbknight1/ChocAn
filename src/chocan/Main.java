9fcb11bc8195646853f970926cea72a8c8b43ba5
Lindsey Bowen <greyjediak@gmail.com>
2025-12-02 00:58:42 -0600

    Made updates and started terminal class. Needs work.

--- src/chocan/Main.java
+++ src/chocan/Main.java
@@ -3,5 +3,10 @@ package chocan;
 class Main {
     public static void main(String[] args) {
         
+
+
+        // Create a new terminal upon startup
+        //Terminal terminal = new Terminal(accounting, directory);
+        //terminal.run();
     }
 }
--- src/chocan/MemberCard.java
+++ src/chocan/MemberCard.java
@@ -1,4 +1,6 @@
 //MemberCard.java written by Lindsey Bowen
+// MemberCard should point to a specific member
+//A member card should automatically be created upon creating a new user
 
 package chocan;
 
@@ -7,6 +9,7 @@ public class MemberCard {
     private String memberLastName;
     private String memberNumber;
 
+    //Main constructor
     public MemberCard(String memberFirstName, String memberLastName, String memberNumber)
     {
         this.memberFirstName = memberFirstName;
@@ -15,6 +18,7 @@ public class MemberCard {
 
     }
 
+    // Getters
     public String getFirstName() { return memberFirstName;}
     public String getLastName() { return memberLastName;}
     public String getMemberNumber() { return memberNumber;}
